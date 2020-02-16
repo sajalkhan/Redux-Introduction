1e24317b88a9afd0fe709bdce6016fbba4857dbc
sajalkhan <sajalhossen@yahoo.com>
2020-02-16 18:10:46 +0600

    add dependencis

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const initialState = ['rabbi', 'rahul'];
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_NAME':
+            return [...state, action.data];
+        case 'DELETE_NAME':
+            let names = [...state];
+            names.splice(action.data, 1);
+            return names;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+window['store'] = store;
+
+const submitHandler = (evt) => {
+    evt.preventDefault();
+    const name = document.getElementById('name').value;
+    const action = {type: 'ADD_NAME', data: name};
+    store.dispatch(action);
+    document.getElementById('name').value = '';
+    document.getElementById('name').focus();
+}
+
+document.getElementById('frmName').onsubmit = submitHandler;
+
+const updateList = () => {
+    let names = store.getState();
+    let list = names.map((name, index)=>'<li class="list-group-item">'
+        + '<button class="btn btn-danger" onclick="deleteName('
+        + index +')">&times;</button>'
+        +name+'</li>');
+    let listItems = list.join('');
+    document.getElementById('namesList').innerHTML = listItems;
+}
+
+updateList();
+store.subscribe(updateList);
+
+window['deleteName'] = (index) => {
+    const action = {type: 'DELETE_NAME', data: index};
+    store.dispatch(action);
+}
